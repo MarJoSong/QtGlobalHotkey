682917ddd3437a18eac7b401b8baa598c61499b6
MarsSong <769200142@qq.com>
2018-11-30 22:48:09 +0800

    添加Qt全局快捷键

---
+++ mainwindow.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "QMessageBox"
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this ->resize(QSize(320,240));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::activated()
+{
+    //QMessageBox::about(0,"提示","检测到快捷键");
+    switch (isVisible()) {
+    case true:
+        this->hide();
+        break;
+    default:
+        this->show();
+        break;
+    }
+
+}
